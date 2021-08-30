754e50b9f5790a743e32470bff21524f896dc547
ВАШЕ_ИМЯAxelSFox <nimwenbadmod@gmail.com>
2021-08-30 13:20:19 +0300

    +

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import Message from './components/Message';
 
 function App(props) {
   return (
-    <div>
+   
     <div className="App">
     <header className="App-header">
       My First React App
       <h3>Hello, {props.name}!</h3>
       </header>
+      <Message name= {props.name}/>
   </div>
-    <Message name="Nadya"/>
-    </div>
+   
+    
   );
 }
 
