e6d64bb2e715209887df36886c86f4794de5daae
Nadya <73992963+axelsfox@users.noreply.github.com>
2021-08-30 13:22:03 +0300

    Revert "Lesson1"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import Message from './components/Message';
 
-function App(props) {
+function App() {
   return (
-   
     <div className="App">
-    <header className="App-header">
-      My First React App
-      <h3>Hello, {props.name}!</h3>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <Message name= {props.name}/>
-  </div>
-   
-    
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,17 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-//import Message from './components/Message';
 import reportWebVitals from './reportWebVitals';
-
-const myName = 'Nadezhda';
-
 
 ReactDOM.render(
   <React.StrictMode>
-    <App name={myName} />
-     
-    
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -20,6 +14,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//<Message name={myName}/>
-
 reportWebVitals();
