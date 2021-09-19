0ae7d2e0329b0b28b4a4e13d7cb9567c7d6b3180
ВАШЕ_ИМЯAxelSFox <nimwenbadmod@gmail.com>
2021-09-19 21:11:24 +0300

    +

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -1,58 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
 import List from '@material-ui/core/List';
-import { makeStyles } from '@material-ui/core/styles';
+import { useStyles, makeStyles } from '@material-ui/core/styles';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom'
-
-const useStyles = makeStyles({
-    root: {
-      width: 500,
-      height: 500,
-      textAlign: 'left',
-      margin: 30,
-      padding: 20,
-    },
-
-    header: {
-      fontSize: 20,
-      fontWeight: 'bold',
-    },
-    textlinks: {
-      textDecoration:'none',
-      color: 'secondary'
-    }
-        
-  });
+import {TextField, Button} from "@material-ui/core";
 
 
-function ChatList({chats}){
-const classes = useStyles(); 
-         
-    return (
-   
-  <Card className={classes.root}>
-   <List >
-    <ListItem>
-          <Typography className={classes.header} color="primary">
-          Chat Name
-          </Typography>
-    </ListItem>
-      
-      {chats.map(chat =>  
-      <ListItem key={chat.id}> 
-          <ListItemText ><Link className={classes.textlinks} to={`/chats/${chat.id}`}>{chat.name}</Link></ListItemText>
-    </ListItem>) }    
-   </List>    
+function ChatList(props){
+  const classes = useStyles(); 
+  const [newname, setnewname] = useState('');
   
-  </Card>  
-    
-     
-    );
+  function entNewChat(event){
+    setnewname(event.target.value)
+  };
+  
+  function handleClick (event){
+    event.preventDefault();
+    props.onClick(newname);
   }
+           
+      return (
+     
+    <Card className={classes.root}>
+     <List >
+      <ListItem>
+            <Typography className={classes.header} color="primary">
+            Chat Name
+            </Typography>
+      </ListItem>
+        
+        {props.chats.map(chat =>  
+        <ListItem key={chat.id}> 
+            <ListItemText >
+              <Link className={classes.textlinks} to={`/chats/${chat.id}`}>{chat.name}</Link>
+            </ListItemText>
+            
+      </ListItem>) } 
+          
+  <ListItem>
+  <form onSubmit={handleClick}>
+          <TextField className={classes.text} id="outlined-basic" value= {newname} onChange={entNewChat} label="New Chat Name" />
+            <Button style={{margin: '20px'}} variant="contained" type="submit" color="secondary">
+              Add
+            </Button>
+      </form>
+  </ListItem>
   
-  export default ChatList;
+  
+     </List>    
+    
+    </Card>  
+      
+       
+      );
+    }
+    
+    export default ChatList;
+  
+   
 
  
